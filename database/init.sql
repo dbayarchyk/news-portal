198d3232bb70dd72d363446c53eef4cea766e1d0
Dzmitry Bayarchyk <dzmitry.bayarchyk@gmail.com@gmail.com>
2020-05-27 16:57:59 +0200

    feat (database): include init.sql into the container

--- database/init.sql
+++ database/init.sql
@@ -1,52 +1,47 @@
--- Database: itdog_database
-
--- DROP DATABASE itdog_database;
-
-CREATE DATABASE itdog_database
-    WITH 
-    OWNER = admin
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.utf8'
-    LC_CTYPE = 'en_US.utf8'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
+\c itdog_database
 
 -- Table: public.user_roles
 
 -- DROP TABLE public.user_roles;
+
+CREATE SEQUENCE user_roles_id_seq;
 
 CREATE TABLE public.user_roles
 (
     id smallint NOT NULL DEFAULT nextval('user_roles_id_seq'::regclass),
     role character varying COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT user_roles_pkey PRIMARY KEY (id)
-)
+);
+
+ALTER SEQUENCE user_roles_id_seq
+    OWNED BY public.user_roles.id;
+
+ALTER TABLE public.user_roles
+    OWNER to admin;
 
 INSERT INTO public.user_roles
-  ( id, name )
+  ( id, role )
 VALUES
   (1, 'VISITOR'), 
   (2, 'USER'), 
   (3, 'WRITER'),
   (4, 'ADMIN');
 
-TABLESPACE pg_default;
-
-ALTER TABLE public.user_roles
-    OWNER to admin;
-
 -- Table: public.permissions
 
 -- DROP TABLE public.permissions;
+
+CREATE SEQUENCE permissions_id_seq;
 
 CREATE TABLE public.permissions
 (
     id bigint NOT NULL DEFAULT nextval('permissions_id_seq'::regclass),
     name character varying COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT permissions_pkey PRIMARY KEY (id)
-)
+);
 
-TABLESPACE pg_default;
+ALTER SEQUENCE permissions_id_seq
+    OWNED BY public.permissions.id;
 
 ALTER TABLE public.permissions
     OWNER to admin;
@@ -54,22 +49,24 @@ ALTER TABLE public.permissions
 INSERT INTO public.permissions
     (id, name)
 VALUES
-    (1, "ARTICLE_VIEW_ALL_DRAFT"),
-    (2, "ARTICLE_VIEW_OWN_DRAFT"),
-    (3, "ARTICLE_VIEW_ALL_PUBLISHED"),
-    (4, "ARTICLE_VIEW_OWN_PUBLISHED")
-    (5, "ARTICLE_VIEW_ALL_PUBLISHED"),
-    (6, "ARTICLE_VIEW_OWN_ARCHIVED");
+    (1, 'ARTICLE_VIEW_ALL_DRAFT'),
+    (2, 'ARTICLE_VIEW_OWN_DRAFT'),
+    (3, 'ARTICLE_VIEW_ALL_PUBLISHED'),
+    (4, 'ARTICLE_VIEW_OWN_PUBLISHED'),
+    (5, 'ARTICLE_VIEW_ALL_ARCHIVED'),
+    (6, 'ARTICLE_VIEW_OWN_ARCHIVED');
 
 -- Table: public.user_roles_to_permissions
 
 -- DROP TABLE public.user_roles_to_permissions;
 
+CREATE SEQUENCE user_roles_to_permissions_id_seq;
+
 CREATE TABLE public.user_roles_to_permissions
 (
     id bigint NOT NULL DEFAULT nextval('user_roles_to_permissions_id_seq'::regclass),
-    user_role_id smallint NOT NULL DEFAULT nextval('user_roles_to_permissions_user_role_id_seq'::regclass),
-    permission_id bigint NOT NULL DEFAULT nextval('user_roles_to_permissions_permission_id_seq'::regclass),
+    user_role_id smallint NOT NULL,
+    permission_id bigint NOT NULL,
     CONSTRAINT user_roles_to_permissions_pkey PRIMARY KEY (id),
     CONSTRAINT user_roles_to_permissions_fkey_permission_id FOREIGN KEY (permission_id)
         REFERENCES public.permissions (id) MATCH SIMPLE
@@ -81,9 +78,10 @@ CREATE TABLE public.user_roles_to_permissions
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
-)
+);
 
-TABLESPACE pg_default;
+ALTER SEQUENCE user_roles_to_permissions_id_seq
+    OWNED BY public.user_roles_to_permissions.id;
 
 ALTER TABLE public.user_roles_to_permissions
     OWNER to admin;
@@ -106,6 +104,8 @@ VALUES
 
 -- DROP TABLE public.users;
 
+CREATE SEQUENCE users_id_seq;
+
 CREATE TABLE public.users
 (
     id bigint NOT NULL DEFAULT nextval('users_id_seq'::regclass),
@@ -119,16 +119,46 @@ CREATE TABLE public.users
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
-)
+);
 
-TABLESPACE pg_default;
+ALTER SEQUENCE users_id_seq
+    OWNED BY public.users.id;
 
 ALTER TABLE public.users
     OWNER to admin;
 
+-- Table: public.article_statuses
+
+-- DROP TABLE public.article_statuses;
+
+CREATE SEQUENCE article_statuses_id_seq;
+
+CREATE TABLE public.article_statuses
+(
+    id smallint NOT NULL DEFAULT nextval('article_statuses_id_seq'::regclass),
+    name character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT article_statuses_pkey PRIMARY KEY (id),
+    CONSTRAINT article_statuses_unique_name UNIQUE (name)
+);
+
+ALTER SEQUENCE article_statuses_id_seq
+    OWNED BY public.article_statuses.id;
+
+ALTER TABLE public.article_statuses
+    OWNER to admin;
+
+INSERT INTO public.article_statuses
+  ( id, name )
+VALUES
+  (1, 'DRAFT'), 
+  (2, 'PUBLISHED'), 
+  (3, 'ARCHIVED');
+
 -- Table: public.articles
 
 -- DROP TABLE public.articles;
+
+CREATE SEQUENCE articles_id_seq;
 
 CREATE TABLE public.articles
 (
@@ -149,33 +179,10 @@ CREATE TABLE public.articles
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
-)
+);
 
-TABLESPACE pg_default;
+ALTER SEQUENCE articles_id_seq
+    OWNED BY public.articles.id;
 
 ALTER TABLE public.articles
     OWNER to admin;
-
--- Table: public.article_statuses
-
--- DROP TABLE public.article_statuses;
-
-CREATE TABLE public.article_statuses
-(
-    id smallint NOT NULL DEFAULT nextval('article_statuses_id_seq'::regclass),
-    name character varying COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT article_statuses_pkey PRIMARY KEY (id),
-    CONSTRAINT article_statuses_unique_name UNIQUE (name)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.article_statuses
-    OWNER to admin;
-
-INSERT INTO public.article_statuses
-  ( id, name )
-VALUES
-  (1, 'DRAFT'), 
-  (2, 'PUBLISHED'), 
-  (3, 'ARCHIVED');
