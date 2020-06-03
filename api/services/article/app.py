0fddfc9c4157b12449b0775386b18c1163060b9c
Dzmitry Bayarchyk <dzmitry.bayarchyk@gmail.com@gmail.com>
2020-06-03 20:33:44 +0200

    feat (api/services/article): remove cors

--- api/services/article/app.py
+++ api/services/article/app.py
@@ -3,10 +3,8 @@ import psycopg2.extras
 import jwt
 import requests
 from flask import Flask, request, make_response
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 DB_HOST = 'postgres'
 DB_NAME = 'itdog_database'
