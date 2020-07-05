7511c065f9672d827225857bab3c3cfe118895d7
Dzmitry Bayarchyk <dzmitry.bayarchyk@gmail.com@gmail.com>
2020-07-05 15:55:34 +0200

    fix (client): show salary chart axis labels  multiple of 1000

--- client/src/components/SalaryRangeChart.svelte
+++ client/src/components/SalaryRangeChart.svelte
@@ -3,8 +3,11 @@
   export let max = 0;
   export let ticks = 4;
 
+  const TICK_RANGE_MULTIPLIER = 1000;
+
   $: range = max - min;
-  $: tickRange = Math.ceil(range / ticks / 5) * 5;
+  $: tickRange =
+    Math.ceil(range / ticks / TICK_RANGE_MULTIPLIER) * TICK_RANGE_MULTIPLIER;
   $: lowerBound = tickRange * Math.floor(min / tickRange);
   $: upperBound = tickRange * Math.ceil(max / tickRange);
 </script>
