db0cee4d71466168980b7235270aaef5033bb7a6
Dzmitry Bayarchyk <dzmitry.bayarchyk@gmail.com@gmail.com>
2020-08-13 21:23:15 +0200

    feat (web): style header layout

--- web/components/header.tsx
+++ web/components/header.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import Link from "next/link";
 
+import styles from "./header.module.scss";
+
 const Header: React.FC = () => {
   const nowDate = new Date();
 
   return (
-    <header>
-      <div>
-        <Link href="/">
-          <a>{nowDate.toLocaleDateString()}</a>
-        </Link>
+    <header className={styles.header}>
+      <div className={styles.headerContent}>
+        <div className={styles.headerContentLeft}>
+          <Link href="/">
+            <a>{nowDate.toLocaleDateString()}</a>
+          </Link>
+        </div>
 
-        <Link href="/">
-          <a>IT Dog</a>
-        </Link>
-
-        <div />
+        <div className={styles.headerContentRight}>
+          <Link href="/">
+            <a>IT Dog</a>
+          </Link>
+        </div>
       </div>
     </header>
   );
