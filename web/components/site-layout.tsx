c4b95b349e8bf030e0271e4bab7da97d4ce69945
Dzmitry Bayarchyk <dzmitry.bayarchyk@gmail.com@gmail.com>
2020-08-13 21:17:32 +0200

    feat (web): style site layout

--- web/components/site-layout.tsx
+++ web/components/site-layout.tsx
@@ -3,15 +3,16 @@ import React from "react";
 import Header from "./header";
 import NavBar from "./nav-bar";
 import Footer from "./footer";
+import styles from "./site-layout.module.scss";
 
 const SiteLayout: React.FC = ({ children }) => {
   return (
-    <>
+    <div className={styles.container}>
       <Header />
       <NavBar />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
