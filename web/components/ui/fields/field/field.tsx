d6ec624d8adf2489773367ad5a3c93bec6e7a0ab
Dzmitry Bayarchyk <dzmitry.bayarchyk@gmail.com@gmail.com>
2020-08-18 21:04:45 +0200

    feat (web): use the stack component in the field

--- web/components/ui/fields/field/field.tsx
+++ web/components/ui/fields/field/field.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import Stack from "../../layouts/stack";
 import style from "./field.module.scss";
 
 const Field: React.FC = ({ children }) => {
-  return <div className={style.field}>{children}</div>;
+  return (
+    <Stack scale="1" className={style.field}>
+      {children}
+    </Stack>
+  );
 };
 
 export default Field;
