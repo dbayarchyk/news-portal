884b6aa85bb6eff67babde6bf4650416492e89bf
Dzmitry Bayarchyk <dzmitry.bayarchyk@gmail.com>
2020-12-09 10:54:19 +0100

    feat (web): style salary report tabs

--- web/components/annual-salary-report.tsx
+++ web/components/annual-salary-report.tsx
@@ -7,6 +7,7 @@ import AnnualSalaryReportData from "./annual-salary-report-data";
 import AnnualSalaryReportMissingData from "./annual-salary-report-missing-data";
 import getAnnualSalaryReport, { AnnualSalaryReportItem } from "../api/market/get-annual-salary-report";
 import fetchAPI from "../api/fetch-api";
+import styles from "./annual-salary-report.module.scss";
 
 type AnnualSalaryReportProps = {
   initialAnnualSalaryReport: AnnualSalaryReportItem[];
@@ -39,13 +40,13 @@ const AnnualSalaryReport: React.FC<AnnualSalaryReportProps> = ({
     <Stack scale="6">
       <Stack scale="4">
         <TabList {...tabState} aria-label="Annual salary report">
-          <Tab {...tabState} id="technology">
+          <Tab {...tabState} className={styles.tab} id="technology">
             Technology
           </Tab>
-          <Tab {...tabState} id="position">
+          <Tab {...tabState} className={styles.tab} id="position">
             Position
           </Tab>
-          <Tab {...tabState} id="city">
+          <Tab {...tabState} className={styles.tab} id="city">
             City
           </Tab>
         </TabList>
