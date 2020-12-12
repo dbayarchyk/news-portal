7fb81e6a46dbdcb64ff183b080c5aae242275ceb
Dzmitry Bayarchyk <dzmitry.bayarchyk@gmail.com>
2020-12-12 16:10:52 +0100

    feat (web): hide user menu on the share salary page

--- web/components/user-menu/user-menu.tsx
+++ web/components/user-menu/user-menu.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useRouter } from "next/router";
 
 import SignInLink from '../sign-in-link';
 import { useAuth } from '../../context/auth';
 import styles from "./user-menu.module.scss";
 
+const PAGES_WITHOUT_USER_SPECIFIC_ACTIONS = ["/salaries/share"];
+
 const UserMenu: React.FC = () => {
   const auth = useAuth();
+  const router = useRouter();
+
+  /* Don't show user specific actions on a page to provide an anonymous feeling. */
+  if (PAGES_WITHOUT_USER_SPECIFIC_ACTIONS.includes(router.pathname)) {
+    return <div/>;
+  }
 
   return (
     <div>
