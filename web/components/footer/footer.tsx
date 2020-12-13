0195340278da705992ae9e9e6cf99a4b263c7fe7
Dzmitry Bayarchyk <dzmitry.bayarchyk@gmail.com>
2020-12-13 14:34:56 +0100

    feat (web): scroll to the top

--- web/components/footer/footer.tsx
+++ web/components/footer/footer.tsx
@@ -1,12 +1,45 @@
 import React from "react";
 
 import Container from "../ui/layouts/container";
+import Center from "../ui/layouts/center";
+import Stack from "../ui/layouts/stack";
+import BodyText from "../ui/body-text"
 import styles from "./footer.module.scss";
 
-const Footer: React.FC = () => {
+type FooterProps = {
+  className?: string;
+  onScrollToTopClick?: () => void;
+};
+
+const Footer: React.FC<FooterProps> = ({
+  className,
+  onScrollToTopClick,
+}) => {
   return (
-    <footer className={styles.footer}>
-      <Container>Footer</Container>
+    <footer className={className}>
+      <Container>
+        <Stack scale="2">
+          {onScrollToTopClick ? (
+            <Center>
+              <button
+                className={styles.scrollToTopButton}
+                type="button"
+                onClick={onScrollToTopClick}
+              >
+                Scroll to the top ☝
+              </button>
+            </Center>
+          ) : null}
+
+          <div className={styles.footerContent}>
+            <Stack scale="1">
+              <BodyText type="secondary">
+                IT Dog Copyright© 2020
+              </BodyText>
+            </Stack>
+          </div>
+        </Stack>
+      </Container>
     </footer>
   );
 };
