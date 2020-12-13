7e973b8ad1572e7dca26751e2063a72a788861af
Dzmitry Bayarchyk <dzmitry.bayarchyk@gmail.com>
2020-12-13 14:34:56 +0100

    feat (web): scroll to the top

--- web/components/site-layout.tsx
+++ web/components/site-layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 import Header from "./header";
 import NavBar from "./nav-bar";
@@ -8,16 +8,55 @@ import Stack from "./ui/layouts/stack";
 import styles from "./site-layout.module.scss";
 
 const SiteLayout: React.FC = ({ children }) => {
+  const scrollToTopQualifierRef = useRef<HTMLDivElement>();
+  const containerRef = useRef<HTMLDivElement>();
+  const [isScrollToTopVisible, setIsScrollToTopVisible] = useState(false);
+
+  useEffect(() => {
+    if (
+      typeof IntersectionObserver !== "function" ||
+      !scrollToTopQualifierRef.current
+    ) {
+      return;
+    }
+
+    const observer = new IntersectionObserver((records) => {
+      for (const record of records) {
+        if (record.isIntersecting) {
+          setIsScrollToTopVisible(false);
+        } else {
+          setIsScrollToTopVisible(true);
+        }
+      }
+    });
+
+    observer.observe(scrollToTopQualifierRef.current);
+
+    return () => {
+      observer.unobserve(scrollToTopQualifierRef.current);
+    };
+  }, []);
+
+  const handleScrollToTopClick = isScrollToTopVisible ? 
+    () => {
+      containerRef.current.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+    : undefined;
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={containerRef}>
       <Header />
+      <div ref={scrollToTopQualifierRef} />
       <Stack scale="8">
         <NavBar />
         <main>
           <Container>{children}</Container>
         </main>
-        <Footer />
       </Stack>
+      <Footer
+        className={styles.footer}
+        onScrollToTopClick={handleScrollToTopClick}
+      />
     </div>
   );
 };
